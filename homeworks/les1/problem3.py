807d327d310ec501b7e5207f7d2dc3f44915c4e1
rheenina <nina.tru94@gmail.com>
2020-09-10 22:05:24 +0800

    changed problem3, problem4

--- homeworks/les1/problem3.py
+++ homeworks/les1/problem3.py
@@ -2,12 +2,10 @@
 #Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
 
 try:
-    n = input("Enter a number ")
-    nn = n + n
-    nnn = nn + n
-    numbers = [n, nn, nnn]
-    numbers = [int(n) for n in numbers]
-    result = numbers[0] + numbers[1] + numbers[2]
+    n = int(input("Enter a number "))
+    nn = n * 11
+    nnn = nn * 10 + n
+    result = n + nn + nnn
     print(result)
 except ValueError:
     print("You've entered not a number!")
--- homeworks/les1/problem4.py
+++ homeworks/les1/problem4.py
@@ -2,16 +2,19 @@
 #Найдите самую большую цифру в числе.
 #Для решения используйте цикл while и арифметические операции.
 
-number = input("Enter a number ")
-numbers = []
-i = 0
-
-if number.isdigit():
-    while i != len(number):
-        numbers.append(number[i])
-        i += 1
-    print(max(numbers))
-else:
+try:
+    number = int(input("Enter a number "))
+    max_number = -1
+    if number > 0:
+        while number > 0:
+            tmp = number % 10
+            number //= 10
+            if tmp > max_number:
+                max_number = tmp
+        print(max_number)
+    else:
+        print("Enter a positive number!")
+except ValueError:
     print("You've entered not a number!")
 
 
